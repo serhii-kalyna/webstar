d3f5140522f2bcdf3564d524b933e2532f201cdc
Serhii Kalyna <serhii_kalyna@ukr.net>
2018-08-30 18:38:08 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,21 +1,41 @@
 $(document).ready(function() {
 
-	$(".down-menu").click(function() {
-		$("ul.dm", this).toggle();
-	});
+	
+	if (window.innerWidth <= 640){
+		$(".down-menu").click(function() {
+			$("ul.dm", this).toggle();
+		});
 
-	$(".drop-mobile-menu").click(function() {
-		$("ul.dm", this).toggle();
-	});
+		$(".drop-mobile-menu").click(function() {
+			$("ul.dm", this).toggle();
+		});
 
-	$(".right-menu1").hover(function() {
-		$("ul.rm1", this).toggle();
-	});
+		$(".right-menu1").click(function() {
+			$("ul.rm1", this).toggle();
+		});
 
-	$(".right-menu2").hover(function() {
-		$("ul.rm2", this).toggle();
-	});
+		$(".right-menu2").click(function() {
+			$("ul.rm2", this).toggle();
+		});
+	}
+	else{
+		$(".down-menu").click(function() {
+			$("ul.dm", this).toggle();
+		});
 
+		$(".drop-mobile-menu").click(function() {
+			$("ul.dm", this).toggle();
+		});
+
+		$(".right-menu1").hover(function() {
+			$("ul.rm1", this).toggle();
+		});
+
+		$(".right-menu2").hover(function() {
+			$("ul.rm2", this).toggle();
+		});
+	}
+	
 	$('.slider').slick({
 		dots: true,
 		infinite: true,
